4863f0e82e0ef0f9c2099e56bb5e3934a546a2a8
Alice Robinson <valkrysa@gmail.com>
2017-01-27 23:39:05 -0800

    background image updates, a bit of prep work

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public float autoLoadNextLevelAfter;
+	
+	void Start(){
+		Invoke("LoadNextLevel", autoLoadNextLevelAfter);
+	}
+
+	public void LoadLevel(string name){
+		Application.LoadLevel(name);
+	}
+	
+	public void LoadNextLevel(){
+		Application.LoadLevel(Application.loadedLevel + 1);
+	}
+	
+	public void QuitRequest(){
+		Application.Quit();
+	}
+	
+}
