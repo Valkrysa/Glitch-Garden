203fd57b47b8e438b4e0236026a5b717db777343
Alice Robinson <valkrysa@gmail.com>
2017-01-29 19:23:37 -0800

    menu structure set up

---
+++ Assets/Scripts/FadeIn.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class FadeIn : MonoBehaviour {
+
+	public float fadeInTime;
+	
+	private Image fadePanel;
+
+	void Start () {
+		fadePanel = GetComponent<Image>();
+		
+		fadePanel.CrossFadeAlpha(0f, fadeInTime, false);
+	}
+	
+	void Update(){
+		if(Time.timeSinceLevelLoad >= fadeInTime){
+			gameObject.SetActive(false);
+		} else {
+			// Ben's Method
+			// I use CrossFadeAlpha instead
+			// float alphaChange = Time.deltaTime / fadeInTime;
+			// currentColor.a -= alphaChange;
+			// fadePanel.color = currentColor;
+		}
+	}
+	
+}
--- Assets/Scripts/MusicManager.cs
+++ Assets/Scripts/MusicManager.cs
@@ -4,14 +4,24 @@ using System.Collections;
 public class MusicManager : MonoBehaviour {
 
 	public AudioClip[] levelMusicChangeArray;
+	
+	private AudioSource audioSource;
+	
+	void Awake(){
+		DontDestroyOnLoad(gameObject);
+	}
 
-	// Use this for initialization
-	void Start () {
-	
+	void Start(){
+		audioSource = GetComponent<AudioSource>();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	void OnLevelWasLoaded(int level) {
+		AudioClip thisLevelMusic = levelMusicChangeArray[level];
+		if(thisLevelMusic){
+			audioSource.clip = thisLevelMusic;
+			audioSource.loop = true;
+			audioSource.Play();
+		}
 	}
+
 }
