be7abdce16cd5dbf1e11ba954ebc59b5f460fcc8
Alice Robinson <valkrysa@gmail.com>
2017-07-02 20:00:14 -0700

    156-160
    health management, animation events/parameters, required components,
    controlling the animator via attached scripts

--- Assets/Scripts/Attacker.cs
+++ Assets/Scripts/Attacker.cs
@@ -1,18 +1,45 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent (typeof (Rigidbody2D))]
 public class Attacker : MonoBehaviour {
 
-	[Range (-1f, 1.5f)]
-	public float walkSpeed;
+	private float currentSpeed;
+	private GameObject currentTarget;
+	private Animator animator;
 
 	// Use this for initialization
 	void Start () {
-	
+		animator = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		transform.Translate (Vector3.left * walkSpeed * Time.deltaTime);
+		transform.Translate (Vector3.left * currentSpeed * Time.deltaTime);
+		if (!currentTarget) {
+			animator.SetBool("isAttacking", false);
+		}
+	}
+	
+	void OnTriggerEnter2D () {
+		Debug.Log (name + " trigger enter");
+	}
+	
+	public void SetSpeed (float speed) {
+		currentSpeed = speed;
+	}
+	
+	// Called from animator at time of actual attack
+	public void StrikeCurrentTarget (float damage) {
+		if (currentTarget) {
+			Health health = currentTarget.GetComponent<Health>();
+			if (health) {
+				health.DealDamage(damage);
+			}
+		}
+	}
+	
+	public void Attack (GameObject obj) {
+		currentTarget = obj;
 	}
 }
---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Health : MonoBehaviour {
+
+	[Range (0f, 100f)]
+	public float health = 100f;
+
+	public void DealDamage (float damage) {
+		health -= damage;
+		
+		if (health <= 0) {
+			// maybe trigger an animation
+			DestroyObject();
+		}
+	}
+	
+	public void DestroyObject () {
+		Destroy(gameObject);
+	}
+}
