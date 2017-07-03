037a58b2b2f90dcfbbf2429245ddb0184116575a
Alice Robinson <valkrysa@gmail.com>
2017-07-03 00:08:23 -0700

    through 161
    through lecture 161, so 53% through this section

--- Assets/Scripts/Defender.cs
+++ Assets/Scripts/Defender.cs
@@ -2,18 +2,5 @@
 using System.Collections;
 
 public class Defender : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-	
-	void OnTriggerEnter2D () {
-		Debug.Log (name + " trigger enter");
-	}
+	// only exists at the moment for checks
 }
---
+++ Assets/Scripts/shooter.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class shooter : MonoBehaviour {
+
+	public GameObject projectile;
+	public GameObject projectileParent;
+	
+	private void Fire () {
+		GameObject newProjectile = Instantiate (projectile) as GameObject;
+		newProjectile.transform.SetParent(projectileParent.transform);
+		
+		GameObject gun = GameObject.Find("Gun"); // could have just dragged this her in the inspector but I wanted to show an alternative to the way ben is doing it
+		
+		if (gun) {
+			newProjectile.transform.position = gun.transform.position;
+		} else {
+			Debug.Log("Could not find child by the name of Gun");
+		}
+	}
+}
