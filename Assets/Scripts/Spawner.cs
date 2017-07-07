ecb263fc94177103dd65f6813d812a00eb7473a9
Alice Robinson <valkrysa@gmail.com>
2017-07-06 23:55:11 -0700

    starting to do spawning

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour {
+
+	public GameObject[] attackerPrefabArray;
+	
+	// Update is called once per frame
+	void Update () {
+		foreach (GameObject thisAttacker in attackerPrefabArray) {
+			if (isTimeToSpawn(thisAttacker)) {
+				Spawn (thisAttacker);
+			}
+		}
+	}
+	
+	public bool isTimeToSpawn (GameObject attacker) {
+		return true;
+	}
+	
+	public void Spawn (GameObject myGameObject) {
+		GameObject newSpawn = Instantiate(myGameObject) as GameObject;
+		newSpawn.transform.SetParent(gameObject.transform);
+		newSpawn.transform.position = gameObject.transform.position;
+	}
+}
