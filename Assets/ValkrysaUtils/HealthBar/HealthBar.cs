07c914b217607ff7c4bbb894a92541f5ba17664b
Alice Robinson <valkrysa@gmail.com>
2017-08-25 21:59:47 -0700

    Added health bars as a prefab

---
+++ Assets/ValkrysaUtils/HealthBar/HealthBar.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour {
+
+    private Health myHealthComponent;
+    private Slider myHealthBar;
+    private float maxHealth;
+
+	// Use this for initialization
+	void Start () {
+        myHealthComponent = GetComponentInParent<Health>();
+        myHealthBar = GetComponentInChildren<Slider>();
+        maxHealth = myHealthComponent.health;
+        myHealthBar.maxValue = maxHealth;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        myHealthBar.value = myHealthComponent.health;
+    }
+}
